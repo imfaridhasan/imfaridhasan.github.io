ff2c8fadfa72087c19a45731028d60c55555e869
Farid Hasan <32639695+imfaridhasan@users.noreply.github.com>
2020-08-05 01:11:38 +0700

    Added Dark Mode
    hopefully i didn't break anything it's 1 AM right now

--- vendor/bootstrap/js/typingeffect.js
+++ vendor/bootstrap/js/typingeffect.js
@@ -49,8 +49,8 @@ var TxtType = function(el, toRotate, period) {
             }
         }
         // INJECT CSS
-          var css = document.createElement("style");
-          css.type = "text/css";
-          css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
-          document.body.appendChild(css);
+        //  var css = document.createElement("style");
+        //  css.type = "text/css";
+        //  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ff0000}";
+        //  document.body.appendChild(css);
     };
